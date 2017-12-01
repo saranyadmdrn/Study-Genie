b2b5ab12003eed62c5308908698b10d0cda5168d
saranyadmdrn <saranyadmrn@gmail.com>
2017-11-30 17:05:47 -0700

    modified criteria'

--- recommender-py/Recommender.py
+++ recommender-py/Recommender.py
@@ -27,9 +27,9 @@ def predict(ratings, similarity):
 def recommendedNotes():
   name = request.args.get('name')
   print name
-  
-  result = mongo.db.events.aggregate(
-  [{ "$match": { "$and": [ {'type': {"$in" : ['note_update','note_open','note_pin','note_copy','note_create','note_share']}}, { 'data.isTrashed': 'false' } ]}},
+  #, { 'data.isTrashed': 'false' }
+  result = list(mongo.db.events.aggregate(
+  [{ "$match": { "$and": [ {'type': {"$in" : ['note_update','note_open','note_pin','note_copy','note_create','note_share']}} ]}},
       {"$group" : {
            "_id" :  {'user':"$user",  'note' : "$data._id"},
            'rating': {"$sum": 1 }
@@ -37,7 +37,7 @@ def recommendedNotes():
       },
         {"$sort":{'_id.user':1}}
    ]
-  )
+  ))
 
   print result
 
@@ -45,14 +45,24 @@ def recommendedNotes():
   notes = mongo.db.events.distinct('data._id')
   data = np.zeros((len(users), len(notes)))
   for i in result:
-  	user = i["_id"]["user"]
-  	note = i["_id"]["note"]
-  	data[users.index(user)][notes.index(note)] = i["rating"]
+    print "fusgukfgsdlfgkfgdls"
+    user = i["_id"]["user"]
+    note = i["_id"]["note"]
+    data[users.index(user)][notes.index(note)] = i["rating"]
 
+  print "matrix"
+  print data
   user_similarity = pairwise_distances(data,metric='cosine')
+  print "similarity"
+  print user_similarity
+
   user_similarity = 1 - user_similarity
+  print user_similarity
   user_prediction = predict(data,user_similarity)
+  print "prediction"
+  
   prediction = np.argsort(-user_prediction[users.index(name)])
+  print user_prediction
   result = [notes[i] for i in prediction]
 
   print result
@@ -60,12 +70,12 @@ def recommendedNotes():
   notes = []
 
   for i in result:
-  	print i
-  	n = mongo.db.Note.find({ "$and": [ {'_id' : i}, {'author' : {"$ne" : name}}, { "$or": [ {'contributors' : {"$in" : [name]}}, {'public' : 'true'}] }] })
-  	if n.count() > 0:
-  		json = json_util.dumps(n)
-  		print json
-  		notes.append(json)
+    print i
+    n = mongo.db.Note.find({ "$and": [ {'_id' : i}, {'author' : {"$ne" : name}}, { "$or": [ {'contributors' : {"$in" : [name]}}, {'public' : 'true'}] }] })
+    if n.count() > 0:
+      json = json_util.dumps(n)
+      print json
+      notes.append(json)
   print notes
   return jsonify({'result' : notes})
 
